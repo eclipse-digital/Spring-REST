7a9a541824b4697a15031f5b173f7810886d3c85
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2022-03-16 22:18:02 +0100

    :tada: Init project from spring initializr

---
+++ src/main/java/uk/co/eclipsegroup/spring_rest_workshops/SpringRestWorkshopsApplication.java
@@ -0,0 +1,13 @@
+package uk.co.eclipsegroup.spring_rest_workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWorkshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWorkshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/eclipsegroup/spring_rest_workshops/SpringRestWorkshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.eclipsegroup.spring_rest_workshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWorkshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
