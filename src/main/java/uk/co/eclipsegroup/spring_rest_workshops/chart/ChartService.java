8f94771f63cf4a979be270b8e655329076699203
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2022-03-29 09:18:35 +0200

    :recycle: Use alternative way of encoding URI

--- src/main/java/uk/co/eclipsegroup/spring_rest_workshops/chart/ChartService.java
+++ src/main/java/uk/co/eclipsegroup/spring_rest_workshops/chart/ChartService.java
@@ -7,7 +7,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uk.co.eclipsegroup.spring_rest_workshops.java.JavaVersion;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,18 +20,35 @@ public class ChartService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     public ResponseEntity<byte[]> createChart(List<JavaVersion> javaVersions) {
-        var javaVersionNames = javaVersions.stream()
-                .map(JavaVersion::getName)
-                .map(name -> "'" + name + "'")
-                .collect(Collectors.joining(","));
+        var javaVersionNames = names(javaVersions);
 
-        var javaVersionValues = javaVersions.stream()
+        var javaVersionValues = values(javaVersions);
+
+        return requestChart(javaVersionNames, javaVersionValues);
+    }
+
+    private String values(List<JavaVersion> javaVersions) {
+        return javaVersions.stream()
                 .map(JavaVersion::getVersion)
                 .map(String::valueOf)
                 .collect(Collectors.joining(","));
+    }
 
-        var uri = UriComponentsBuilder.fromUriString("https://quickchart.io/chart?c={type:'bar',data:{labels:[" + javaVersionNames + "], datasets:[{label:'Version',data:[" + javaVersionValues + "]}]}}")
-                .build().encode().toUri();
-        return restTemplate.exchange(uri, HttpMethod.GET, null, byte[].class);
+    private String names(List<JavaVersion> javaVersions) {
+        return javaVersions.stream()
+                .map(JavaVersion::getName)
+                .map(name -> "'" + name + "'")
+                .collect(Collectors.joining(","));
+    }
+
+    private ResponseEntity<byte[]> requestChart(String javaVersionNames, String javaVersionValues) {
+        try {
+            var encodedQuery = URLEncoder.encode("{type:'bar',data:{labels:[" + javaVersionNames + "], datasets:[{label:'Version',data:[" + javaVersionValues + "]}]}}", StandardCharsets.UTF_8.toString());
+            return restTemplate.exchange(new URI("https://quickchart.io/chart?c=" + encodedQuery), HttpMethod.GET, null, byte[].class);
+        } catch (UnsupportedEncodingException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 }
