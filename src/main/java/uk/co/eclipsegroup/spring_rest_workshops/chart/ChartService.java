120e0a9d46fb18dc1208b0962a3723cef5188a93
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2022-04-05 10:05:03 +0200

    :recycle: Use POST method rather than GET to generate chart

--- src/main/java/uk/co/eclipsegroup/spring_rest_workshops/chart/ChartService.java
+++ src/main/java/uk/co/eclipsegroup/spring_rest_workshops/chart/ChartService.java
@@ -1,10 +1,13 @@
 package uk.co.eclipsegroup.spring_rest_workshops.chart;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import uk.co.eclipsegroup.spring_rest_workshops.chart.request.Chart;
+import uk.co.eclipsegroup.spring_rest_workshops.chart.request.ChartRequest;
+import uk.co.eclipsegroup.spring_rest_workshops.chart.request.Data;
+import uk.co.eclipsegroup.spring_rest_workshops.chart.request.Datasets;
 import uk.co.eclipsegroup.spring_rest_workshops.java.JavaVersion;
 
 import java.util.List;
@@ -15,17 +18,31 @@ public class ChartService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     public ResponseEntity<byte[]> createChart(List<JavaVersion> javaVersions) {
-        var javaVersionNames = javaVersions.stream()
-                .map(JavaVersion::getName)
-                .collect(Collectors.joining(",", "'", "'"));
+        var chartRequest = fromJavaVersions(javaVersions);
+        var httpRequest = new HttpEntity<>(chartRequest, json());
+        return restTemplate.exchange("https://quickchart.io/chart", HttpMethod.POST, httpRequest, byte[].class);
+    }
 
-        var javaVersionValues = javaVersions.stream()
+    private HttpHeaders json() {
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        return httpHeaders;
+    }
+
+    ChartRequest fromJavaVersions(List<JavaVersion> javaVersions) {
+        return new ChartRequest(new Chart("bar", new Data(namesFrom(javaVersions), List.of(new Datasets("Version", valuesFrom(javaVersions))))));
+    }
+
+    private List<String> valuesFrom(List<JavaVersion> javaVersions) {
+        return javaVersions.stream()
                 .map(JavaVersion::getVersion)
                 .map(String::valueOf)
-                .collect(Collectors.joining(","));
+                .collect(Collectors.toList());
+    }
 
-        var uri = UriComponentsBuilder.fromUriString("https://quickchart.io/chart?c={type:'bar',data:{labels:[" + javaVersionNames + "], datasets:[{label:'Version',data:[" + javaVersionValues + "]}]}}")
-                .build().encode().toUri();
-        return restTemplate.exchange(uri, HttpMethod.GET, null, byte[].class);
+    private List<String> namesFrom(List<JavaVersion> javaVersions) {
+        return javaVersions.stream()
+                .map(JavaVersion::getName)
+                .collect(Collectors.toList());
     }
 }
